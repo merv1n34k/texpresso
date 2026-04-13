aaf372c111838d0b888fd294195927af892cfc98
Oleksii Stroganov <merv1n@proton.me>
2026-04-14 02:47:26 +0300

    fix: handle relative paths in open/close and wait for engine in test mode

--- src/frontend/main.c
+++ src/frontend/main.c
@@ -788,12 +788,15 @@ static void interpret_open(struct persistent_state *ps,
                            const void *data,
                            int size)
 {
-  int go_up = 0;
-  path = relative_path(path, ps->doc_path, &go_up);
-  if (go_up > 0)
+  if (path[0] == '/')
   {
-    fprintf(stderr, "[command] open %s: file has a different root, skipping\n", path);
-    return;
+    int go_up = 0;
+    path = relative_path(path, ps->doc_path, &go_up);
+    if (go_up > 0)
+    {
+      fprintf(stderr, "[command] open %s: file has a different root, skipping\n", path);
+      return;
+    }
   }
 
   fileentry_t *e = send(find_file, ui->eng, ps->ctx, path);
@@ -843,12 +846,15 @@ static void interpret_close(struct persistent_state *ps,
                             ui_state *ui,
                             const char *path)
 {
-  int go_up = 0;
-  path = relative_path(path, ps->doc_path, &go_up);
-  if (go_up > 0)
+  if (path[0] == '/')
   {
-    fprintf(stderr, "[command] close %s: file has a different root, skipping\n", path);
-    return;
+    int go_up = 0;
+    path = relative_path(path, ps->doc_path, &go_up);
+    if (go_up > 0)
+    {
+      fprintf(stderr, "[command] close %s: file has a different root, skipping\n", path);
+      return;
+    }
   }
 
   fileentry_t *e = send(find_file, ui->eng, ps->ctx, path);
@@ -1497,7 +1503,9 @@ bool texpresso_main(struct persistent_state *ps)
           break;
       }
     }
-    if (ps->initialize_only)
+    if (ps->initialize_only &&
+        (send(page_count, ui->eng) > 0 ||
+         (send(get_status, ui->eng) == DOC_TERMINATED && stdin_eof)))
     {
       fprintf(stderr, "[info] Initialize mode: terminating engine process\n");
       quit = 1;
